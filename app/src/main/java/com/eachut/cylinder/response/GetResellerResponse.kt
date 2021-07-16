5f23f84d52926c839350dec7709efa118e8920c6
bhanubhakta56 <abromeo114@gmail.com>
2021-07-16 20:14:37 +0545

    Get Reseller

--- app/src/main/java/com/eachut/cylinder/response/GetResellerResponse.kt
+++ app/src/main/java/com/eachut/cylinder/response/GetResellerResponse.kt
@@ -2,7 +2,7 @@ package com.eachut.cylinder.response
 
 import com.eachut.cylinder.entity.Reseller
 
-data class GetAllProductResponse (
+data class GetResellerResponse (
     val success:Boolean?=null,
     val message:String?=null,
     val data: MutableList<Reseller>?=null
