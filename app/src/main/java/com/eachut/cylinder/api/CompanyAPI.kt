321f8606576b4aab5cf30e420e884f1e9c1e5ae2
bhanubhakta56 <abromeo114@gmail.com>
2021-07-16 20:37:19 +0545

    get company API repository and resonse

--- app/src/main/java/com/eachut/cylinder/api/CompanyAPI.kt
+++ app/src/main/java/com/eachut/cylinder/api/CompanyAPI.kt
@@ -1,6 +1,7 @@
 package com.eachut.cylinder.api
 import com.eachut.cylinder.entity.Company
-import com.eachut.cylinder.response.AddNewCompanyResponse
+import com.eachut.cylinder.entity.Reseller
+import com.eachut.cylinder.response.*
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -10,4 +11,24 @@ interface CompanyAPI {
     suspend fun newCompany(
         @Body company: Company
     ):Response<AddNewCompanyResponse>
+
+    @GET("/companyList")
+    suspend fun allCompanyList(
+//        @Header("Authorization") token : String,
+    ): Response<GetAllCompanyResponse>
+
+    @GET("/companyList/{id}")
+    suspend fun companyList(
+//        @Header("Authorization") token : String,
+        @Path ("id") id: String,
+    ): Response<GetCompanyResponse>
+
+
+    @PUT("/company/update/{id}")
+    suspend fun updateReseller(
+//        @Header("Authorization") token: String,
+        @Path ("id") id: String,
+        @Body company: Company
+    ): Response<AddNewCompanyResponse>
+
 }
--- app/src/main/java/com/eachut/cylinder/repository/CompanyRepository.kt
+++ app/src/main/java/com/eachut/cylinder/repository/CompanyRepository.kt
@@ -3,16 +3,40 @@ package com.eachut.cylinder.repository
 import com.eachut.cylinder.api.CompanyAPI
 import com.eachut.cylinder.api.MyApiRequest
 import com.eachut.cylinder.api.ServiceBuilder
-import com.eachut.cylinder.response.AddNewResellerResponse
+import com.eachut.cylinder.entity.Company
+import com.eachut.cylinder.entity.Reseller
+import com.eachut.cylinder.response.*
 
 class CompanyRepository: MyApiRequest() {
     private val companyAPI =
         ServiceBuilder.buildService(CompanyAPI::class.java)
 
-//    //    Add New Company
-//    suspend fun newCompany(company: Company): AddNewCompanyResponse {
-//        return apiRequest {
-//            companyAPI.newCompany(company)
-//        }
-//    }
+    //    Add New Company
+    suspend fun newCompany(company: Company): AddNewCompanyResponse {
+        return apiRequest {
+            companyAPI.newCompany(company)
+        }
+    }
+
+    //get Reseller
+    suspend fun allCompanyList(): GetAllCompanyResponse {
+        return apiRequest {
+            companyAPI.allCompanyList()
+        }
+    }
+
+    //get single reseller
+    suspend fun companyList(id:String): GetCompanyResponse {
+        return apiRequest {
+            companyAPI.companyList(id)
+        }
+    }
+
+    //update reseller
+    suspend fun updateReseller(id:String, company: Company):AddNewCompanyResponse{
+        return apiRequest {
+            companyAPI.updateReseller(id, company)
+        }
+    }
+
 }
