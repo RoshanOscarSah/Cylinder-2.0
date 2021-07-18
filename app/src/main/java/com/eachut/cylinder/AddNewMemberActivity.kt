38947d8a14e5b891478a11516066451ed3ec26df
RoshanOscarSah <RoshanSah729@gmail.com>
2021-07-18 16:42:06 +0545

    member ko admin ra employee milako

--- app/src/main/java/com/eachut/cylinder/AddNewMemberActivity.kt
+++ app/src/main/java/com/eachut/cylinder/AddNewMemberActivity.kt
@@ -1,41 +1,122 @@
 package com.eachut.cylinder
 
+import android.animation.ObjectAnimator
+import android.app.Activity
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import kotlinx.coroutines.*
 
 class AddNewMemberActivity : AppCompatActivity() {
 
-    private lateinit var etUsername : TextView
-    private lateinit var lstname : TextView
-    private lateinit var phonenum : TextView
-    private lateinit var address : TextView
-    private lateinit var cylincom : TextView
-    private lateinit var loginbtn : TextView
 
+    private lateinit var etMFname : EditText
+    private lateinit var etMLname : EditText
+    private lateinit var etMPhone : EditText
+    private lateinit var etMAddress : EditText
+    private lateinit var cylincom : EditText
+    private lateinit var btnAddProfile : TextView
+    private lateinit var ivSendCheck : ImageView
+    private lateinit var ivRecieveCheck : ImageView
+    private lateinit var llAdmin : LinearLayout
+    private lateinit var llEmploye : LinearLayout
+    private lateinit var adminOrEmployee : LinearLayout
+    private lateinit var commission : LinearLayout
+
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_member)
 
-        etUsername = findViewById(R.id.etUsername)
-        lstname = findViewById(R.id.lstname)
-        phonenum = findViewById(R.id.phonenum)
-        address = findViewById(R.id.address)
+        etMFname = findViewById(R.id.etMFname)
+        etMLname = findViewById(R.id.etMLname)
+        etMPhone = findViewById(R.id.etMPhone)
+        etMAddress = findViewById(R.id.etMAddress)
         cylincom = findViewById(R.id.cylincom)
-        loginbtn = findViewById(R.id.loginbtn)
+        btnAddProfile = findViewById(R.id.btnAddProfile)
+        ivSendCheck = findViewById(R.id.ivSendCheck)
+        ivRecieveCheck = findViewById(R.id.ivRecieveCheck)
+        llAdmin = findViewById(R.id.llAdmin)
+        llEmploye = findViewById(R.id.llEmploye)
+        adminOrEmployee = findViewById(R.id.adminOrEmployee)
+        commission = findViewById(R.id.commission)
 
-        loginbtn.setOnClickListener {
 
-            val first_name = etUsername.text.toString()
-            val last_name = lstname.text.toString()
-            val phone_number = phonenum.text.toString()
-            val address = address.text.toString()
-            val comission_percent = cylincom.text.toString()
 
-//            val user = User(first_name = first_name, last_name = last_name,phone_number = phone_number,address = address,comission_percent = comission_percent)
+//Admin / Employee
+        llAdmin.setOnClickListener { view ->
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(ivSendCheck, "translationX", 0f).apply {
+                duration = 200
+                start()
+            }
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(ivRecieveCheck, "translationX", -250f).apply {
+                duration = 200
+                start()
+            }
+
+
+
+            ivRecieveCheck.isVisible = false
+            ivSendCheck.isVisible = true
+            commission.isVisible = false
+
+            adminOrEmployee.setContentDescription("ADMIN")
+
+
+            llEmploye.setBackgroundTintList(ColorStateList.valueOf(resources.getColor(R.color.sendreceive_fade)));
+            llAdmin.setBackgroundTintList(ColorStateList.valueOf(resources.getColor(R.color.sendreceive)));
+        }
+
+        llEmploye.setOnClickListener { view ->
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(ivSendCheck, "translationX", 460f).apply {
+                duration = 200
+                start()
+            }
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(ivRecieveCheck, "translationX", 210f).apply {
+                duration = 200
+                start()
+            }
+
+            ivRecieveCheck.isVisible = true
+            ivSendCheck.isVisible = false
+            commission.isVisible = true
+
+            adminOrEmployee.setContentDescription("EMPLOYEE")
+
+            llEmploye.setBackgroundTintList(ColorStateList.valueOf(resources.getColor(R.color.sendreceive)));
+            llAdmin.setBackgroundTintList(ColorStateList.valueOf(resources.getColor(R.color.sendreceive_fade)));
+
+        }
+
+
+
+
+//        btnAddProfile.setOnClickListener {
+
+//            val etMFname = etMFname.text.toString()
+//            val etMLname = etMLname.text.toString()
+//            val etMPhone = etMPhone.text.toString()
+//            val etMAddress = etMAddress.text.toString()
+//            val cylincom = cylincom.text.toString()
+//        val status = adminOrEmployee.getContentDescription()
+
+
+//            val user = User(first_name = etMFname, last_name = etMLname,phone_number = etMPhone,address = etMAddress,comission_percent = comission_percent)
 
 //            CoroutineScope(Dispatchers.IO).launch {
 //                try {
@@ -68,7 +149,7 @@ class AddNewMemberActivity : AppCompatActivity() {
 //                    }
 //                }
 //                }
-            }
+//            }
 
         }
     }
