184c4990656c4df67a663942fe902d3eb0a86a9c
RoshanOscarSah <RoshanSah729@gmail.com>
2021-07-19 09:34:16 +0545

    entry first page design completed

---
+++ app/src/main/java/com/eachut/cylinder/PrenameActivity.kt
@@ -0,0 +1,12 @@
+package com.eachut.cylinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eachut.cylinder.R
+
+class PrenameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prename)
+    }
+}
