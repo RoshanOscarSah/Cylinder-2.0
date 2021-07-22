46e605d74a95936c70ddd0f88d4d732bce03fdad
AnishNepal <anishnepal62@gmail.com>
2021-07-22 15:17:21 +0545

    PDF Generator

--- app/src/main/java/com/eachut/cylinder/ReceiptActivity.kt
+++ app/src/main/java/com/eachut/cylinder/ReceiptActivity.kt
@@ -1,11 +1,16 @@
 package com.eachut.cylinder
 
+import android.Manifest
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.os.Environment
+import android.util.DisplayMetrics
+import android.view.WindowManager
 import com.eachut.cylinder.entity.Company
 import com.eachut.cylinder.entity.CompanyStock
 import com.eachut.cylinder.entity.Reseller
@@ -18,9 +23,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import kotlin.Exception
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Canvas
+import android.graphics.pdf.PdfDocument.PageInfo
+import android.net.Uri
+import android.os.Environment.getExternalStoragePublicDirectory
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
 
 class ReceiptActivity : AppCompatActivity() {
+    private lateinit var llpdf : LinearLayout
+    private lateinit var rlbottom : RelativeLayout
     private lateinit var txttitlename : TextView
     private lateinit var txtaddress : TextView
     private lateinit var txtCname : TextView
@@ -40,12 +61,19 @@ class ReceiptActivity : AppCompatActivity() {
     private lateinit var txtCylinder : TextView
     private lateinit var txtCash : TextView
     private lateinit var btnSubmit:TextView
+    private lateinit var btnDownload:Button
 
+    private val permissions = arrayOf(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+    private lateinit var bitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receipt)
 
+        llpdf = findViewById(R.id.llpdf)
+        rlbottom = findViewById(R.id.rlbottom)
         txttitlename = findViewById(R.id.txttitlename)
         txtaddress = findViewById(R.id.txtaddress)
         txtCname = findViewById(R.id.txtCname)
@@ -64,7 +92,18 @@ class ReceiptActivity : AppCompatActivity() {
         txtSold = findViewById(R.id.txtSold)
         txtCylinder = findViewById(R.id.txtCylinder)
         txtCash = findViewById(R.id.txtCash)
+        btnDownload = findViewById(R.id.btnDownload)
         btnSubmit = findViewById(R.id.btnSubmit)
+
+        btnDownload.setOnClickListener(View.OnClickListener {
+            Log.d("size", " " + llpdf.getWidth() + "  " + llpdf.getWidth())
+            bitmap = loadBitmapFromView(llpdf, llpdf.getWidth(), llpdf.getHeight())
+            createPdf()
+        })
+
+        if (!hasPermissions()) {
+            requestPermission()
+        }
 //new comment
 
         val status = intent.getStringExtra("status")
@@ -157,10 +196,68 @@ class ReceiptActivity : AppCompatActivity() {
 
         }
 
-
-
     }
 
+    private fun createPdf() {
+        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+        val displaymetrics = DisplayMetrics()
+        this.windowManager.defaultDisplay.getMetrics(displaymetrics)
+        val hight = displaymetrics.heightPixels.toFloat()
+        val width = displaymetrics.widthPixels.toFloat()
+        val convertHighet = hight.toInt()
+        val convertWidth = width.toInt()
 
+        val document = PdfDocument()
+        val pageInfo = PdfDocument.PageInfo.Builder(convertWidth, convertHighet, 1).create()
+        val page = document.startPage(pageInfo)
+        val canvas = page.canvas
+        val paint = Paint()
+        canvas.drawPaint(paint)
+        bitmap = Bitmap.createScaledBitmap(bitmap!!, convertWidth, convertHighet, true)
+        paint.color = Color.BLUE
+        canvas.drawBitmap(bitmap, 0f, 0f, null)
+        document.finishPage(page)
 
+        // write the document content
+        val targetPdf =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        val filePath: File
+        filePath = File(targetPdf, "CylinderReceipt.pdf")
+        try {
+            with(document) { writeTo(FileOutputStream(filePath)) }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "$e", Toast.LENGTH_LONG).show()
+        }
+
+        // close the document
+        document.close()
+        Toast.makeText(this, "Receipt PDF is created!!!", Toast.LENGTH_LONG).show()
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            permissions, 1
+        )
+    }
+
+    private fun hasPermissions(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
+    }
+
+    companion object {
+        fun loadBitmapFromView(v: View?, width: Int, height: Int): Bitmap {
+            val b = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            val c = Canvas(b)
+            v!!.draw(c)
+            return b
+        }
+    }
 }
