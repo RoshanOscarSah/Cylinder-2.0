dfcef4587e2bda0f6791caef775ae0e7e34a9058
AnishNepal <anishnepal62@gmail.com>
2021-08-30 11:17:30 +0545

    Notification API

--- app/src/main/java/com/eachut/cylinder/api/NotificationAPI.kt
+++ app/src/main/java/com/eachut/cylinder/api/NotificationAPI.kt
@@ -1,8 +1,21 @@
 package com.eachut.cylinder.api
 
 import com.eachut.cylinder.entity.NotificationHistory
+import com.eachut.cylinder.response.AddNotificationResponse
+import com.eachut.cylinder.response.GetNotificationHistory
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface NotificationAPI {
+    //Adding Notification
+    @POST("/notification")
+    suspend fun addNotification(
+        @Body notificationHistory: NotificationHistory
+    ):Response<AddNotificationResponse>
+
+    @GET("/notification")
+    suspend fun notificationHistorylist(
+    ):Response<GetNotificationHistory>
 }
