3b1ce0cbc61e9e734f2d899bb07f808f4eb4abd5
AnishNepal <anishnepal62@gmail.com>
2021-09-05 20:47:02 +0545

    Changes in Profile Fragment

--- app/src/main/java/com/eachut/cylinder/Object/ResellerStockDetails.kt
+++ app/src/main/java/com/eachut/cylinder/Object/ResellerStockDetails.kt
@@ -3,6 +3,15 @@ package com.eachut.cylinder.Object
 import com.eachut.cylinder.entity.ResellerStock
 
 object ResellerStockDetails {
+    var shortedResellerStockList = mutableListOf<ResellerStock>()
+
+    fun setResellerStockList(resellerStock : MutableList<ResellerStock>){
+        shortedResellerStockList = resellerStock
+    }
+    fun getResellerStockList():MutableList<ResellerStock>{
+        return shortedResellerStockList
+    }
+
     var data = ResellerStock()
     var flag = false
     fun setResellerStockDetails(resellerStock: ResellerStock){
